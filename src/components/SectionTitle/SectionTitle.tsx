39b810c069da84b2f27f3d639d0eb287fb15eb7b
AmletixQ <nazarovamridin308@gmail.com>
2025-03-26 14:46:18 +0300

    Create Title component for sections

---
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,14 @@
+import { FC, PropsWithChildren } from "react";
+
+import styles from "./SectionTitle.module.scss";
+import Hill from "../icons/Hill/Hill";
+
+const SectionTitle: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Hill />
+      <h2>{children}</h2>
+    </div>
+  );
+};
+export default SectionTitle;
