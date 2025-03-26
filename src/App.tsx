e15f21eb65bbcdcbcfda593bc60e6ac98297087f
AmletixQ <nazarovamridin308@gmail.com>
2025-03-26 14:23:05 +0300

    Complete about us section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Header from "./components/Header/Header";
 
+import styles from "./App.module.scss";
+import AboutUs from "./sections/AboutUs/AboutUs";
+
 const App = () => {
   return (
     <>
       <Header />
+
+      <main className={styles.app}>
+        <AboutUs />
+      </main>
     </>
   );
 };
---
+++ src/sections/AboutUs/AboutUs.tsx
@@ -0,0 +1,32 @@
+import Button from "../../Button/Button";
+import Container from "../../components/Container/Container";
+import styles from "./AboutUs.module.scss";
+
+const AboutUs = () => {
+  return (
+    <Container>
+      <div className={styles.aboutUs}>
+        <img
+          className={styles.aboutUs__image}
+          src="/images/aboutUs.png"
+          alt=""
+        />
+        <div className={styles.aboutUs__content}>
+          <div className={styles.aboutUs__content__typography}>
+            <h1>Палаточный лагерь «Седой Кшурт»</h1>
+            <p>
+              Место, где чистый горный воздух, потрясающие виды и настоящая
+              свобода сливаются воедино. Здесь вас ждут захватывающие походы,
+              уютные вечера у костра и полное единение с природой. Независимо от
+              того, ищете ли вы активный отдых или тихий уголок для
+              перезагрузки, у нас есть всё для комфортного пребывания в горах.
+            </p>
+          </div>
+
+          <Button color="primary">Забронировать место</Button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+export default AboutUs;
