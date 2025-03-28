1509948d834c8d6fafa54cd2c44a2023422e830f
AmletixQ <nazarovamridin308@gmail.com>
2025-03-28 15:29:56 +0300

    Fix locate input component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "../../Input/Input";
+import Input from "../../components/Input/Input";
 import Button from "../Button/Button";
 
 import styles from "./Form.module.scss";
