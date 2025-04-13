b87f3c7544a68cb7f881b58bd78d379de8847c3d
AmletixQ <nazarovamridin308@gmail.com>
2025-04-13 18:54:06 +0300

    Refactor section title component on base new design

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,27 +1,10 @@
 import { FC, PropsWithChildren } from "react";
-
 import styles from "./SectionTitle.module.scss";
 import Hill from "../icons/Hill/Hill";
-import { cva, VariantProps } from "class-variance-authority";
 
-const title = cva(styles.container, {
-  variants: {
-    side: {
-      left: styles.left,
-      right: styles.right,
-      center: styles.center,
-    },
-  },
-});
-
-type ITitleProps = VariantProps<typeof title>;
-
-const SectionTitle: FC<ITitleProps & PropsWithChildren> = ({
-  side,
-  children,
-}) => {
+const SectionTitle: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className={title({ side })}>
+    <div className={styles.container}>
       <Hill />
       <h2>{children}</h2>
     </div>
