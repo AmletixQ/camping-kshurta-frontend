6b70a73fda7ad0436ac037b73ea2c38feaa52917
AmletixQ <nazarovamridin308@gmail.com>
2025-04-14 16:03:19 +0300

    Refactor SmallText component

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,15 +2,12 @@ import { FC, useState } from "react";
 
 import Container from "../Container/Container";
 import Arrow from "../icons/Arrow/Arrow";
+import SmallText from "../SmallText/SmallText";
 
 import styles from "./Accordion.module.scss";
+import { IAccordion } from "../../types/accordion.interface";
 
-interface IAccordionProps {
-  title: string;
-  description: string;
-}
-
-const Accordion: FC<IAccordionProps> = ({ title, description }) => {
+const Accordion: FC<IAccordion> = ({ title, small, description }) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -21,7 +18,11 @@ const Accordion: FC<IAccordionProps> = ({ title, description }) => {
             className={styles.accordion__title}
             onClick={() => setOpen((prev) => !prev)}
           >
-            <h3>{title}</h3>
+            <h3>
+              {title}
+              {small && <SmallText>{small}</SmallText>}
+            </h3>
+
             <div className={styles.accordion__button}>
               <Arrow />
             </div>
--- src/types/accordion.interface.ts
+++ src/types/accordion.interface.ts
@@ -1,4 +1,5 @@
 export interface IAccordion {
   title: string;
+  small?: string;
   description: string;
 }
