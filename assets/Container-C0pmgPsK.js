01d2785db9c662f1a6e158a172b245667299ed66
AmletixQ <nazarovamridin308@gmail.com>
2025-04-15 20:56:43 +0300

    Updates

---
+++ assets/Container-C0pmgPsK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DX_w6EVb.js";const r="_container_1zsj5_1",e={container:r},c=({children:n,id:t,className:s})=>o.jsx("div",{id:t,className:`${e.container} ${s}`,children:n});export{c as C};
--- assets/SmallText-BIyO7U_u.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CxXuR3Nk.js";const l="_smalltext_6j634_1",a={smalltext:l},m=({children:t})=>s.jsx("span",{className:a.smalltext,children:t});export{m as S};
