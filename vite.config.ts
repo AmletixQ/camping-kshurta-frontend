400f94cd5c0ab30d27ea474f69a73fe14b9e7212
AmletixQ <nazarovamridin308@gmail.com>
2025-04-18 22:12:40 +0300

    Solution for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/camping-kshurta-frontend",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE || "/",
+  };
 });
