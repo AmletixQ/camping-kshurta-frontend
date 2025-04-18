1377f46bb39db18c6c64f0aed95d4a3fbc5ce4ee
AmletixQ <nazarovamridin308@gmail.com>
2025-04-18 22:18:12 +0300

    Updates

--- assets/Container-jKDBQQGZ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DoH7HiF3.js";const r="_container_1zsj5_1",e={container:r},c=({children:n,id:t,className:s})=>o.jsx("div",{id:t,className:`${e.container} ${s}`,children:n});export{c as C};
