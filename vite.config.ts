5cab7bde082b185cad105215d1d124a754f31173
AmletixQ <nazarovamridin308@gmail.com>
2025-04-28 20:11:08 +0300

    First step of SEO-optimize

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      Sitemap({ hostname: "https://camping-kshurta.ru" }),
+    ],
     base:
       env.VITE_NODE_ENV === "production" ? "/camping-kshurta-frontend" : "/",
   };
