93a49fa1882c5cebd62b3c5c9b684767c8a34d7b
AmletixQ <nazarovamridin308@gmail.com>
2025-05-01 22:14:24 +0300

    Fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,13 +12,13 @@ const Header = () => {
       <div className={styles.header}>
         <Logo />
 
-        <div className={styles.header__anchors}>
+        <nav className={styles.header__anchors}>
           {anchors.map(({ href, title }) => (
             <a href={href} key={title}>
               {title}
             </a>
           ))}
-        </div>
+        </nav>
         <div className={styles.header__contacts}>
           <a href="tel:+79094755913" className={styles.header__phonenumber}>
             +7 909 475 59 13
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
-    plugins: [
-      react(),
-      Sitemap({ hostname: "https://camping-kshurta.ru" }),
-    ],
+    plugins: [react()],
     base:
       env.VITE_NODE_ENV === "production" ? "/camping-kshurta-frontend" : "/",
   };
